f9b8fb6b783cadfeb047de7ba9b1a32e796213e2
Mathias Tahas <Mathias@mathiass-mbp.fritz.box>
2016-04-30 22:34:45 +0200

    Initial Commit

---
+++ BoxOfHorrorMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BoxOfHorrorMac
+//
+//  Created by Mathias Tahas on 30.04.16.
+//  Copyright © 2016 Mathias Tahas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BoxOfHorrorMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BoxOfHorrorMac
+//
+//  Created by Mathias Tahas on 30.04.16.
+//  Copyright © 2016 Mathias Tahas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
